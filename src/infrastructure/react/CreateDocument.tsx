d6dd7d110e3b64e15cedfaa7c11944db49f0c04d
Yota Toyama <raviqqe@gmail.com>
2019-05-05 04:58:41 +0000

    Show textarea always

--- src/infrastructure/react/CreateDocument.tsx
+++ src/infrastructure/react/CreateDocument.tsx
@@ -5,30 +5,19 @@ interface IProps {
 }
 
 export const CreateDocument = ({ createDocument }: IProps) => {
-  const [creating, setCreating] = useState(false);
   const [text, setText] = useState("");
-
-  if (!creating) {
-    return <button onClick={() => setCreating(true)}>New</button>;
-  }
 
   return (
     <div>
-      <textarea
-        autoFocus={true}
-        onChange={event => setText(event.target.value)}
-        value={text}
-      />
+      <textarea onChange={event => setText(event.target.value)} value={text} />
       <button
         onClick={async () => {
-          setCreating(false);
           setText("");
           await createDocument(text);
         }}
       >
-        Create
+        +
       </button>
-      <button onClick={() => setCreating(false)}>Cancel</button>
     </div>
   );
 };
