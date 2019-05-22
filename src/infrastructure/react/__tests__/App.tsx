2e0eead505ed749852cdb9d887b2ae111a79fda5
Yota Toyama <raviqqe@gmail.com>
2019-05-22 03:22:22 +0000

    Test sign-in

--- src/infrastructure/react/__tests__/App.tsx
+++ src/infrastructure/react/__tests__/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, waitForDomChange } from "react-testing-library";
+import { fireEvent, render, waitForDomChange } from "react-testing-library";
 import { IDocument } from "../../../domain/document";
 import { App } from "../App";
 
@@ -43,4 +43,29 @@ it("renders after a user signs in", async () => {
   await waitForDomChange(result);
 
   expect(result.container).toMatchSnapshot();
+});
+
+it("goes to a home screen when a user signs in", async () => {
+  const { asFragment, container } = render(
+    <App
+      createDocument={async () => ({} as IDocument)}
+      initialize={async () => ({ signedIn: false })}
+      insertImage={async () => "url"}
+      listDocuments={async function*() {
+        yield [];
+      }}
+      signIn={async () => true}
+      signOut={async () => false}
+      updateDocument={async () => ({} as IDocument)}
+      repositoryURL="url"
+    />
+  );
+
+  await waitForDomChange({ container });
+
+  fireEvent.click(container.querySelector("button") as Element);
+
+  await waitForDomChange({ container });
+
+  expect(asFragment()).toMatchSnapshot();
 });
