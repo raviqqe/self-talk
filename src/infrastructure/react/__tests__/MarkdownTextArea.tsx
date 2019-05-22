5399dfb39982519b0747268c4885d0fecdf3878f
Yota Toyama <raviqqe@gmail.com>
2019-05-22 03:10:08 +0000

    Test textarea components

--- src/infrastructure/react/__tests__/MarkdownTextArea.tsx
+++ src/infrastructure/react/__tests__/MarkdownTextArea.tsx
@@ -1,16 +1,87 @@
 import React from "react";
-import { create } from "react-test-renderer";
+import { Simulate } from "react-dom/test-utils";
+import { render, waitForDomChange } from "react-testing-library";
 import { MarkdownTextArea } from "../MarkdownTextArea";
 
 it("renders", () => {
   expect(
-    create(
+    render(
       <MarkdownTextArea
         insertImage={async () => "url"}
         onSubmit={async () => undefined}
         setText={() => undefined}
         text="foo"
       />
-    ).toJSON()
+    ).container
   ).toMatchSnapshot();
+});
+
+it("pastes an image as a link", async () => {
+  const setText = jest.fn();
+
+  const { container } = render(
+    <MarkdownTextArea
+      insertImage={async () => "result"}
+      onSubmit={async () => undefined}
+      setText={setText}
+      text="foo"
+    />
+  );
+
+  // TODO: Use fireEvent.paste when it is fixed.
+  Simulate.paste(
+    container.firstElementChild as Element,
+    {
+      clipboardData: {
+        items: [{ type: "image/png", getAsFile: () => new File([], "foo.png") }]
+      }
+    } as any
+  );
+
+  await waitForDomChange({ container });
+
+  expect(setText.mock.calls).toEqual([["result"]]);
+});
+
+it("does not paste anything if there is no clipboard data", async () => {
+  const setText = jest.fn();
+
+  const { container } = render(
+    <MarkdownTextArea
+      insertImage={async () => "result"}
+      onSubmit={async () => undefined}
+      setText={setText}
+      text="foo"
+    />
+  );
+
+  // TODO: Use fireEvent.paste when it is fixed.
+  Simulate.paste(container.firstElementChild as Element, {} as any);
+
+  await waitForDomChange({ container });
+
+  expect(setText.mock.calls).toHaveLength(0);
+});
+
+it("does not paste anything if there is no images in clipboard data", async () => {
+  const setText = jest.fn();
+
+  const { container } = render(
+    <MarkdownTextArea
+      insertImage={async () => "result"}
+      onSubmit={async () => undefined}
+      setText={setText}
+      text="foo"
+    />
+  );
+
+  // TODO: Use fireEvent.paste when it is fixed.
+  Simulate.paste(
+    container.firstElementChild as Element,
+    { clipboardData: { items: [] } } as any
+  );
+
+  await waitForDomChange({ container });
+
+  expect(setText.mock.calls).toHaveLength(0);
 });
--- src/infrastructure/react/__tests__/TextArea.tsx
+++ src/infrastructure/react/__tests__/TextArea.tsx
@@ -1,9 +1,30 @@
 import React from "react";
-import { create } from "react-test-renderer";
+import { fireEvent, render } from "react-testing-library";
 import { TextArea } from "../TextArea";
 
 it("renders", () => {
   expect(
-    create(<TextArea onSubmit={() => undefined} />).toJSON()
+    render(<TextArea onSubmit={() => undefined} />).container
   ).toMatchSnapshot();
+});
+
+it("triggers a submit callback on Enter+Shift", () => {
+  const onSubmit = jest.fn();
+  const { container } = render(<TextArea onSubmit={onSubmit} />);
+
+  fireEvent.keyDown(container.firstElementChild as Element, {
+    keyCode: 13,
+    shiftKey: true
+  });
+
+  expect(onSubmit.mock.calls).toHaveLength(1);
+});
+
+it("does not trigger a submit callback on Enter", () => {
+  const onSubmit = jest.fn();
+  const { container } = render(<TextArea onSubmit={onSubmit} />);
+
+  fireEvent.keyDown(container.firstElementChild as Element, { keyCode: 13 });
+
+  expect(onSubmit.mock.calls).toHaveLength(0);
 });
