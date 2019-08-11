7154d03c20d2503e79b2f939d848dd85ed88cbdc
Yota Toyama <raviqqe@gmail.com>
2019-08-10 20:47:19 -0700

    Test images in Markdown

--- src/infrastructure/react/__tests__/Markdown.tsx
+++ src/infrastructure/react/__tests__/Markdown.tsx
@@ -1,7 +1,36 @@
+import { render, fireEvent } from "@testing-library/react";
 import { create } from "react-test-renderer";
 import React from "react";
 import { Markdown } from "../Markdown";
 
 it("renders", () => {
   expect(create(<Markdown># Foo</Markdown>).toJSON()).toMatchSnapshot();
+});
+
+it("renders images", () => {
+  expect(
+    create(<Markdown>![foo](http://bar/baz.png)</Markdown>).toJSON()
+  ).toMatchSnapshot();
+});
+
+it("opens an image when it is clicked", () => {
+  const spy = jest.spyOn(window, "open");
+
+  const { container } = render(<Markdown>![foo](http://bar/baz.png)</Markdown>);
+  fireEvent.click(container.querySelector("img") as Element);
+
+  expect(spy).toBeCalledTimes(1);
+  spy.mockRestore();
+});
+
+it("does not open a linked image even when it is clicked", () => {
+  const spy = jest.spyOn(window, "open");
+
+  const { container } = render(
+    <Markdown>[![foo](http://bar/baz.png)](http://bar/blah)</Markdown>
+  );
+  fireEvent.click(container.querySelector("img") as Element);
+
+  expect(spy).toBeCalledTimes(0);
+  spy.mockRestore();
 });
