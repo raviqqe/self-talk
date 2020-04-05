8e1efc839394f86c50c659d26feb24ec27a5741d
Yota Toyama <raviqqe@gmail.com>
2020-04-05 21:29:54 +0000

    Fix build

--- src/application/__tests__/sign-in-manager.ts
+++ src/application/__tests__/sign-in-manager.ts
@@ -8,7 +8,9 @@ beforeEach(() => {
   signInMock = jest.fn();
   signInManager = new SignInManager(({
     signIn: signInMock,
-  } as unknown) as IAuthenticationController);
+  } as
+    unknown) as
+    IAuthenticationController);
 });
 
 it("signs in", async () => {
