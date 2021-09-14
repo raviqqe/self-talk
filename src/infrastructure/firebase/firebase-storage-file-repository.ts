f3a5083af2095f190584cf252dca6e302dbbf063
Yota Toyama <raviqqe@gmail.com>
2021-09-14 01:14:25 +0000

    Fix linting

--- src/infrastructure/firebase/firebase-storage-file-repository.ts
+++ src/infrastructure/firebase/firebase-storage-file-repository.ts
@@ -12,7 +12,7 @@ export class FirebaseStorageFileRepository implements IFileRepository {
       cacheControl: `max-age=${60 * 60 * 24 * 365}`,
     });
 
-    return (await child.getDownloadURL()) as string;
+    return child.getDownloadURL();
   }
 
   private files(): firebase.storage.Reference {
