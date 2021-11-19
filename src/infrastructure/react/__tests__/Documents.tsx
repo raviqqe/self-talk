159a82a2dc37ea2343d5c99a633041bb05f59464
Yota Toyama <raviqqe@gmail.com>
2021-11-18 23:04:22 -0800

    Fix tests (#1414)

--- src/infrastructure/react/__tests__/Documents.tsx
+++ src/infrastructure/react/__tests__/Documents.tsx
@@ -1,44 +1,63 @@
-import { create } from "react-test-renderer";
+import { act, render, waitFor } from "@testing-library/react";
 import { Documents } from "../Documents";
 
-it("renders", () => {
-  expect(
-    create(
-      <Documents
-        documents={[{ id: "id", text: "text" }]}
-        insertFiles={async () => ""}
-        listDocuments={async () => {}}
-        listMoreDocuments={async () => {}}
-        updateDocument={async () => {}}
-      />
-    ).toJSON()
-  ).toMatchSnapshot();
+let listDocuments = jest.fn();
+const wait = () => waitFor(() => expect(listDocuments).toBeCalled());
+
+beforeEach(() => {
+  listDocuments.mockReset().mockResolvedValue(undefined);
 });
 
-it("renders with no documents", () => {
-  expect(
-    create(
-      <Documents
-        documents={[]}
-        insertFiles={async () => ""}
-        listDocuments={async () => {}}
-        listMoreDocuments={async () => {}}
-        updateDocument={async () => {}}
-      />
-    ).toJSON()
-  ).toMatchSnapshot();
+it("renders", async () => {
+  await act(async () => {
+    expect(
+      render(
+        <Documents
+          documents={[{ id: "id", text: "text" }]}
+          insertFiles={async () => ""}
+          listDocuments={listDocuments}
+          listMoreDocuments={async () => {}}
+          updateDocument={async () => {}}
+        />
+      ).container.firstChild
+    ).toMatchSnapshot();
+
+    await wait();
+  });
 });
 
-it("renders with documents not loaded yet", () => {
-  expect(
-    create(
-      <Documents
-        documents={null}
-        insertFiles={async () => ""}
-        listDocuments={async () => {}}
-        listMoreDocuments={async () => {}}
-        updateDocument={async () => {}}
-      />
-    ).toJSON()
-  ).toMatchSnapshot();
+it("renders with no documents", async () => {
+  await act(async () => {
+    expect(
+      render(
+        <Documents
+          documents={[]}
+          insertFiles={async () => ""}
+          listDocuments={listDocuments}
+          listMoreDocuments={async () => {}}
+          updateDocument={async () => {}}
+        />
+      ).container.firstChild
+    ).toMatchSnapshot();
+
+    await wait();
+  });
+});
+
+it("renders with documents not loaded yet", async () => {
+  await act(async () => {
+    expect(
+      render(
+        <Documents
+          documents={null}
+          insertFiles={async () => ""}
+          listDocuments={listDocuments}
+          listMoreDocuments={async () => {}}
+          updateDocument={async () => {}}
+        />
+      ).container.firstChild
+    ).toMatchSnapshot();
+
+    await wait();
+  });
 });
