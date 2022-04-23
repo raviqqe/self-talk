3428f0713132bbc78acdd450a312697be8929f86
Yota Toyama <raviqqe@gmail.com>
2022-04-23 03:40:15 +0000

    Fix more

--- src/infrastructure/react/__tests__/Home.tsx
+++ src/infrastructure/react/__tests__/Home.tsx
@@ -1,82 +1,102 @@
 import {
   act,
-  cleanup,
   fireEvent,
   render,
+  RenderResult,
   waitFor,
 } from "@testing-library/react";
 import { Home } from "../Home";
 
-afterEach(cleanup);
+it("renders", () => {
+  let result: RenderResult | undefined;
 
-it("renders", async () => {
-  await act(async () => {
-    expect(
-      render(
-        <Home
-          createDocument={async () => {}}
-          documents={[]}
-          insertFiles={async () => ""}
-          listDocuments={async () => {}}
-          listMoreDocuments={async () => {}}
-          signOut={async () => {}}
-          updateDocument={async () => {}}
-        />
-      ).container.firstChild
-    ).toMatchSnapshot();
+  act(() => {
+    result = render(
+      <Home
+        createDocument={async () => {}}
+        documents={[]}
+        insertFiles={async () => ""}
+        listDocuments={async () => {}}
+        listMoreDocuments={async () => {}}
+        signOut={async () => {}}
+        updateDocument={async () => {}}
+      />
+    );
   });
+
+  expect(result?.container.firstChild).toMatchSnapshot();
 });
 
 it("creates a document", async () => {
   const createDocument = jest.fn(async () => {});
+  let result: RenderResult | undefined;
 
-  const { container, getByLabelText } = render(
-    <Home
-      createDocument={createDocument}
-      documents={[]}
-      insertFiles={async () => ""}
-      listDocuments={async () => {}}
-      listMoreDocuments={async () => {}}
-      signOut={async () => {}}
-      updateDocument={async () => {}}
-    />
-  );
-
-  await waitFor(() => expect(container.querySelector("textarea")).toBeTruthy());
-
-  fireEvent.change(container.querySelector("textarea") as HTMLTextAreaElement, {
-    target: { value: "foo" },
+  act(() => {
+    result = render(
+      <Home
+        createDocument={createDocument}
+        documents={[]}
+        insertFiles={async () => ""}
+        listDocuments={async () => {}}
+        listMoreDocuments={async () => {}}
+        signOut={async () => {}}
+        updateDocument={async () => {}}
+      />
+    );
   });
 
-  fireEvent.click(getByLabelText("Create"));
+  await waitFor(() =>
+    expect(result?.container.querySelector("textarea")).toBeTruthy()
+  );
+
+  act(() => {
+    fireEvent.change(
+      result?.container.querySelector("textarea") as HTMLTextAreaElement,
+      { target: { value: "foo" } }
+    );
+
+    if (result) {
+      fireEvent.click(result?.getByLabelText("Create"));
+    }
+  });
 
   expect(createDocument.mock.calls).toHaveLength(1);
 });
 
 it("updates a document", async () => {
   const updateDocument = jest.fn(async () => {});
+  let result: RenderResult | undefined;
 
-  const { container, getByLabelText } = render(
-    <Home
-      createDocument={async () => {}}
-      documents={[{ id: "", text: "" }]}
-      insertFiles={async () => ""}
-      listDocuments={async () => {}}
-      listMoreDocuments={async () => {}}
-      signOut={async () => {}}
-      updateDocument={updateDocument}
-    />
-  );
-
-  await waitFor(() => expect(getByLabelText("Edit")).toBeTruthy());
-
-  fireEvent.click(getByLabelText("Edit"));
-
-  fireEvent.change(container.querySelector("textarea") as HTMLTextAreaElement, {
-    target: { value: "foo" },
+  act(() => {
+    result = render(
+      <Home
+        createDocument={async () => {}}
+        documents={[{ id: "", text: "" }]}
+        insertFiles={async () => ""}
+        listDocuments={async () => {}}
+        listMoreDocuments={async () => {}}
+        signOut={async () => {}}
+        updateDocument={updateDocument}
+      />
+    );
   });
 
-  fireEvent.click(getByLabelText("Save"));
+  await waitFor(() => expect(result?.getByLabelText("Edit")).toBeTruthy());
+
+  act(() => {
+    if (result) {
+      fireEvent.click(result.getByLabelText("Edit"));
+    }
+
+    fireEvent.change(
+      result?.container.querySelector("textarea") as HTMLTextAreaElement,
+      { target: { value: "foo" } }
+    );
+
+    if (result) {
+      fireEvent.click(result?.getByLabelText("Save"));
+    }
+  });
 
   expect(updateDocument.mock.calls).toHaveLength(1);
 });
