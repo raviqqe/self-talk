047c928f38add24406b8c4e7b9d378a724863594
Yota Toyama <raviqqe@gmail.com>
2022-10-04 23:51:21 -0700

    Fix

--- src/infrastructure/react/MarkdownTextArea.test.tsx
+++ src/infrastructure/react/MarkdownTextArea.test.tsx
@@ -21,77 +21,77 @@ it("renders", () => {
   ).toMatchSnapshot();
 });
 
-// it("pastes an image as a link", async () => {
-//   const setText = vi.fn();
-//   let result: RenderResult | undefined;
+it("pastes an image as a link", async () => {
+  const setText = vi.fn();
+  let result: RenderResult | undefined;
 
-//   act(() => {
-//     result = render(
-//       <MarkdownTextArea
-//         insertFiles={async () => "result"}
-//         onSubmit={async () => {}}
-//         setText={setText}
-//         text="foo"
-//       />
-//     );
-//   });
+  act(() => {
+    result = render(
+      <MarkdownTextArea
+        insertFiles={async () => "result"}
+        onSubmit={async () => {}}
+        setText={setText}
+        text="foo"
+      />
+    );
+  });
 
-//   fireEvent.paste(
-//     result?.container.firstElementChild as Element,
-//     {
-//       clipboardData: {
-//         items: [
-//           { getAsFile: () => new File([], "foo.png", { type: "image/png" }) },
-//         ],
-//       },
-//     } as never
-//   );
+  fireEvent.paste(
+    result?.container.firstElementChild as Element,
+    {
+      clipboardData: {
+        items: [
+          { getAsFile: () => new File([], "foo.png", { type: "image/png" }) },
+        ],
+      },
+    } as never
+  );
 
-//   await waitFor(() => expect(setText.mock.calls).toEqual([["result"]]));
-// });
+  await waitFor(() => expect(setText.mock.calls).toEqual([["result"]]));
+});
 
-// it("does not paste anything if there is no files in clipboard data", async () => {
-//   const setText = vi.fn();
-//   let result: RenderResult | undefined;
+it("does not paste anything if there is no files in clipboard data", async () => {
+  const setText = vi.fn();
+  let result: RenderResult | undefined;
 
-//   act(() => {
-//     result = render(
-//       <MarkdownTextArea
-//         insertFiles={async () => "result"}
-//         onSubmit={async () => {}}
-//         setText={setText}
-//         text="foo"
-//       />
-//     );
-//   });
+  act(() => {
+    result = render(
+      <MarkdownTextArea
+        insertFiles={async () => "result"}
+        onSubmit={async () => {}}
+        setText={setText}
+        text="foo"
+      />
+    );
+  });
 
-//   fireEvent.paste(result?.container.firstElementChild as Element, {
-//     clipboardData: { items: [] },
-//   });
+  fireEvent.paste(result?.container.firstElementChild as Element, {
+    clipboardData: { items: [] },
+  });
 
-//   await waitFor(() => expect(setText.mock.calls).toHaveLength(0));
-// });
+  await waitFor(() => expect(setText.mock.calls).toHaveLength(0));
+});
 
-// it("drops an image as a link", async () => {
-//   const setText = vi.fn();
-//   let result: RenderResult | undefined;
+it("drops an image as a link", async () => {
+  const setText = vi.fn();
+  let result: RenderResult | undefined;
 
-//   act(() => {
-//     result = render(
-//       <MarkdownTextArea
-//         insertFiles={async () => "result"}
-//         onSubmit={async () => {}}
-//         setText={setText}
-//         text="foo"
-//       />
-//     );
-//   });
+  act(() => {
+    result = render(
+      <MarkdownTextArea
+        insertFiles={async () => "result"}
+        onSubmit={async () => {}}
+        setText={setText}
+        text="foo"
+      />
+    );
+  });
 
-//   fireEvent.drop(result?.container.firstElementChild as Element, {
-//     dataTransfer: {
-//       files: [new File([], "foo.png", { type: "image/png" })],
-//     },
-//   });
+  fireEvent.drop(result?.container.firstElementChild as Element, {
+    dataTransfer: {
+      files: [new File([], "foo.png", { type: "image/png" })],
+    },
+  });
 
-//   await waitFor(() => expect(setText.mock.calls).toEqual([["result"]]));
-// });
+  await waitFor(() => expect(setText.mock.calls).toEqual([["result"]]));
+});
