7be07f913478790b4290d42ae75852eeb9e7f6a3
Yota Toyama <raviqqe@gmail.com>
2022-10-05 18:18:03 -0700

    Fix deployment (#1766)
    * Fix
    
    * Install VitePWA
    
    * Remove service worker
    
    * Fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        short_name: "SelfTalk",
+        name: "SelfTalk",
+        icons: [{ src: "icon.svg", sizes: "any" }],
+        start_url: ".",
+        display: "standalone",
+        theme_color: "khaki",
+        background_color: "darkkhaki",
+      },
+    }),
+  ],
   test: { environment: "jsdom" },
 });
