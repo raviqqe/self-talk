7f54166a00c1d64607bf38465568d372a7c861c0
Yota Toyama <raviqqe@gmail.com>
2023-02-07 16:26:26 +0800

    Fix linting

--- src/application/test/authentication-controller.ts
+++ src/application/test/authentication-controller.ts
@@ -2,7 +2,7 @@ import { Mocked, vi } from "vitest";
 import { IAuthenticationController } from "../authentication-controller";
 
 export const authenticationController: Mocked<IAuthenticationController> = {
+  isSignedIn: vi.fn(async () => false),
   signIn: vi.fn(async () => {}),
   signOut: vi.fn(async () => {}),
-  isSignedIn: vi.fn(async () => false),
 };
