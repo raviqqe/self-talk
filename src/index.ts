e87dfd90e8323577adfe29f332eb537123ef6b27
Yota Toyama <raviqqe@gmail.com>
2023-02-07 15:43:43 +0800

    Update lock file

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { DocumentUpdater } from "./application/document-updater";
 import { SignInManager } from "./application/sign-in-manager";
 import { SignOutManager } from "./application/sign-out-manager";
 import { TextFileInserter } from "./application/text-file-inserter";
-import configuration from "./configuration.json";
+import * as configuration from "./configuration.json";
 import { AlertMessagePresenter } from "./infrastructure/alert-message-presenter";
 import { AuthenticationPresenter } from "./infrastructure/authentication-presenter";
 import { BuiltinConfirmationController } from "./infrastructure/builtin-confirmation-controller";
