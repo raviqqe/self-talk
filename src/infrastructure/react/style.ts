5dbb109fc7b75655baf623eb7771da43fa52ac61
Yota Toyama <raviqqe@gmail.com>
2024-02-12 15:31:59 +0900

    Fix box-shadow

--- src/infrastructure/react/style.ts
+++ src/infrastructure/react/style.ts
@@ -22,6 +22,6 @@ export const globalStyle = css`
   }
 `;
 
-export const boxShadow = css`
-  box-shadow: 0rem 0.2rem 0.2rem rgba(0, 0, 0, 0.2);
-`;
+export const boxShadow = {
+  boxShadow: "0rem 0.2rem 0.2rem rgba(0, 0, 0, 0.2)",
+};
