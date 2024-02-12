2c1143fb9fbe2e2ee4bbd2bdea22396b25eea23c
Yota Toyama <raviqqe@gmail.com>
2024-02-12 15:27:33 +0900

    Fix

--- src/infrastructure/react/style.ts
+++ src/infrastructure/react/style.ts
@@ -2,21 +2,23 @@ import { css } from "@linaria/core";
 import { black, yellow } from "./style/colors.js";
 
 export const globalStyle = css`
-  body {
-    background: ${yellow};
-    margin: 0;
-    padding: 0;
-    color: ${black};
-    font-family: Roboto, sans-serif;
-    line-height: 1.4;
-    font-size: 16px;
-    height: 100vh;
-    width: 100vw;
-    overflow: hidden;
-  }
+  :global {
+    body {
+      background: ${yellow};
+      margin: 0;
+      padding: 0;
+      color: ${black};
+      font-family: Roboto, sans-serif;
+      line-height: 1.4;
+      font-size: 16px;
+      height: 100vh;
+      width: 100vw;
+      overflow: hidden;
+    }
 
-  #root {
-    overflow-x: hidden;
+    #root {
+      overflow-x: hidden;
+    }
   }
 `;
 
