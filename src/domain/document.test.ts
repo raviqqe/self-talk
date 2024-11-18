3dea97925d151c696ff55de51283311a4c9d0256
Yota Toyama <raviqqe@gmail.com>
2024-11-18 22:23:23 +0900

    Fix

--- src/domain/document.test.ts
+++ src/domain/document.test.ts
@@ -1,9 +1,5 @@
 import { describe, expect, it } from "vitest";
-import {
-  type Document,
-  formatDocument,
-  validateDocument,
-} from "./document.js";
+import { type Document, formatDocument, validateDocument } from "./document.js";
 
 describe("formatDocument", () => {
   it("removes extra spaces", async () => {
