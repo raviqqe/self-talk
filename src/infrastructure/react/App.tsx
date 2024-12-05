4390d24cb9450229836afc3cec3787ac52bb5088
Yota Toyama <raviqqe@gmail.com>
2024-12-05 22:57:14 +0900

    Fix

--- src/infrastructure/react/App.tsx
+++ src/infrastructure/react/App.tsx
@@ -1,10 +1,10 @@
 import { styled } from "@linaria/react";
 import { PulseLoader } from "react-spinners";
 import { useAsync } from "react-use";
+import { applicationInitializer } from "../../main/application-initializer.js";
 import { Home, type Props as HomeProps } from "./Home.js";
 import { Landing, type Props as LandingProps } from "./Landing.js";
 import { white } from "./style/colors.js";
-import { applicationInitializer } from "../../main/application-initializer.js";
 
 const LoaderContainer = styled.div`
   display: flex;
