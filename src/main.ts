8e655f6d66be5d4658ba0efb8204eb31f91b8383
Yota Toyama <raviqqe@gmail.com>
2024-12-06 10:31:01 +0900

    Fix

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import configuration from "./configuration.json" with { type: "json" };
 import { ReactRenderer } from "./infrastructure/react.js";
 import { authenticationPresenter } from "./main/authentication-presenter.js";
 import { documentPresenter } from "./main/document-presenter.js";
@@ -11,11 +10,10 @@ try {
     throw new Error("no root element");
   }
 
-  new ReactRenderer(
-    element,
-    [authenticationPresenter, documentPresenter],
-    configuration.repositoryUrl,
-  ).render();
+  new ReactRenderer(element, [
+    authenticationPresenter,
+    documentPresenter,
+  ]).render();
 
   // Disable default behavior on drop events.
   window.ondragover = (event: DragEvent) => event.preventDefault();
