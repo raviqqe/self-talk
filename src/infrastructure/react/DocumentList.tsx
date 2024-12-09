df33a6a163a31dda2c4550c36097c0530e97ab4a
Yota Toyama <raviqqe@gmail.com>
2024-12-09 23:44:35 +0900

    Fix

--- src/infrastructure/react/DocumentList.tsx
+++ src/infrastructure/react/DocumentList.tsx
@@ -1,16 +1,11 @@
 import { styled } from "@linaria/react";
 import { useStore } from "@nanostores/react";
-import { useAsync } from "@raviqqe/react-hooks";
-import { defaultImport } from "default-import";
-import { useEffect, useState } from "react";
-import defaultUseInfiniteScroll from "react-infinite-scroll-hook";
-import { usePrevious } from "react-use";
+import { useAsync, useInfiniteScroll } from "@raviqqe/react-hooks";
+import { useState } from "react";
 import { documentLister } from "../../main/document-lister.js";
 import { documentPresenter } from "../../main/document-presenter.js";
 import { Document } from "./Document.js";
 import { Loader } from "./Loader.js";
-
-const useInfiniteScroll = defaultImport(defaultUseInfiniteScroll);
 
 const Container = styled.div`
   display: flex;
@@ -32,35 +27,19 @@ const StyledDocument = styled(Document)`
 
 export const DocumentList = (): JSX.Element => {
   const documents = useStore(documentPresenter.documents);
-  const [loading, setLoading] = useState(false);
   const [done, setDone] = useState(false);
 
-  useAsync(async () => {
-    setLoading(true);
-    await documentLister.list();
-    setLoading(false);
-  }, []);
+  useAsync(() => documentLister.list(), []);
 
-  const [ref] = useInfiniteScroll({
-    hasNextPage: !done,
-    loading,
-    onLoadMore: async () => {
-      setLoading(true);
-      await documentLister.listMore();
-      setLoading(false);
-    },
-  });
+  const ref = useInfiniteScroll<HTMLDivElement>(async () => {
+    const documents = documentPresenter.documents.get();
+    await documentLister.listMore();
+    const newDocuments = documentPresenter.documents.get();
 
-  const oldLoading = usePrevious(loading);
-  const [length, setLength] = useState(0);
-
-  useEffect(() => {
-    if (!oldLoading && loading) {
-      setLength(documents?.length ?? 0);
-    } else if (oldLoading && !loading && documents?.length === length) {
-      setDone(true);
+    if (newDocuments && documents) {
+      setDone(newDocuments.length === documents.length);
     }
-  }, [documents, length, loading, oldLoading]);
+  });
 
   return (
     <Container>
