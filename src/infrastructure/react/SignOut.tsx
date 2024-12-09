7b5df50ae2263c1befbbc3478ecc4d10d12eae44
Yota Toyama <raviqqe@gmail.com>
2024-12-09 21:34:19 +0900

    Fix

--- src/infrastructure/react/SignOut.tsx
+++ src/infrastructure/react/SignOut.tsx
@@ -1,6 +1,6 @@
 import { MdExitToApp } from "react-icons/md";
-import { CircleButton } from "./CircleButton.js";
 import { signOutManager } from "../../main/sign-out-manager.js";
+import { CircleButton } from "./CircleButton.js";
 
 export const SignOut = (): JSX.Element => (
   <CircleButton onClick={() => signOutManager.signOut()} secondary>
