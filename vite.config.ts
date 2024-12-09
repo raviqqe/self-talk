e8dd1e2864ee6f3dccf233c611c4859d230b69ba
Yota Toyama <raviqqe@gmail.com>
2024-12-09 22:16:13 +0900

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { reactRouter } from "@react-router/dev/vite";
 import wywInJs from "@wyw-in-js/vite";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
@@ -12,6 +13,20 @@ export default defineConfig({
           importAttributesKeyword: "with",
         },
         presets: ["@babel/preset-typescript", "@babel/preset-react"],
+      },
+    }),
+    VitePWA({
+      manifest: {
+        short_name: "SelfTalk",
+        name: "SelfTalk",
+        icons: [{ src: "icon.svg", sizes: "any" }],
+        start_url: ".",
+        display: "standalone",
+        theme_color: "darkkhaki",
+        background_color: "darkkhaki",
+      },
+      workbox: {
+        navigateFallbackDenylist: [/^\/__/],
       },
     }),
   ],
