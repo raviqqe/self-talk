339da675b71a67a2e6daf4cfd66dbcb2f38b5c69
Yota Toyama <raviqqe@gmail.com>
2024-12-10 17:43:58 +0900

    Fix

--- src/infrastructure/firebase/firebase-initializer.ts
+++ src/infrastructure/firebase/firebase-initializer.ts
@@ -19,6 +19,12 @@ export class FirebaseInitializer {
   public initialize(): FirebaseApp {
     const app = initializeApp(this.configuration);
 
+    initializeFirestore(app, {
+      localCache: persistentLocalCache({
+        tabManager: persistentMultipleTabManager(),
+      }),
+    });
+
     return app;
   }
 }
