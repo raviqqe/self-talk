5d46c9c64b3938ec4b18f65a3ef08cf793297c24
Yota Toyama <raviqqe@gmail.com>
2024-12-10 13:55:15 +0900

    Fix

--- src/infrastructure/react/style.ts
+++ src/infrastructure/react/style.ts
@@ -1,3 +1,12 @@
+export const black = "#222";
+export const darkGrey = "dimgrey";
+export const darkYellow = "darkkhaki";
+export const grey = "grey";
+export const lightGrey = "darkgrey";
+export const red = "tomato";
+export const white = "white";
+export const yellow = "khaki";
+
 export const boxShadow = {
   boxShadow: "0rem 0.2rem 0.2rem rgba(0, 0, 0, 0.2)",
 };
