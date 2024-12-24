216c1e4e110bdac703096be6098c629d08830758
Yota Toyama <raviqqe@gmail.com>
2024-12-24 21:25:00 +0900

    Fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
-import { type PluginOption } from "vite";
 
 export default defineConfig({
-  plugins: [react() as PluginOption[]],
+  plugins: [react()],
   test: {
     environment: "jsdom",
     include: ["src/**/*.test.ts{,x}"],
