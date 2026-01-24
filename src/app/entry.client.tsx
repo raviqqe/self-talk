1b924fda9c99fc20eeff74c9593c436b65308429
Yota Toyama <raviqqe@gmail.com>
2026-01-23 23:52:11 -0800

    fix

--- src/app/entry.client.tsx
+++ src/app/entry.client.tsx
@@ -1,6 +1,10 @@
-import { startTransition, StrictMode } from "react";
+import * as sentry from "@sentry/react-router";
+import { StrictMode, startTransition } from "react";
 import { hydrateRoot } from "react-dom/client";
 import { HydratedRouter } from "react-router/dom";
+import configuration from "../configuration.json" with { type: "json" };
+
+sentry.init(configuration.sentry);
 
 startTransition(() => {
   hydrateRoot(
